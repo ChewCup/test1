d94cc9d8289a2afe00b05cee397d7926efbd4e49
ChewCup <72440370+ChewCup@users.noreply.github.com>
2020-10-15 16:07:02 +0200

    updated

---
+++ test1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Viktor";
+            string street = "Folkborgsvägen"; 
+            string zip_city = "603 80 Norrköping";
+
+            //set background color
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Clear();
+
+            Console.SetCursorPosition(15, 15);
+            Console.WriteLine(name);
+            Console.SetCursorPosition(15, 16);
+            Console.WriteLine(street);
+            Console.SetCursorPosition(15, 17);
+            Console.WriteLine(zip_city);
+
+
+            //stop
+            Console.ReadKey();
+
+             
+
+
+
+
+        }
+    }
+}
